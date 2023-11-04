8731654f8b19f382173e24d5857fa005b873165e
SoltanMustafa <soltan.mustafali290@gmail.com>
2023-11-04 12:17:15 +0400

    Done.HowItShouldBeDone

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
+import Model from './model.js';
 import View from './view.js';
-import Model from './model.js'
-
+import Controller from './controller.js';
 
 const players = [
   {
@@ -17,55 +17,14 @@ const players = [
   }
 ];
 
-
-function init() {
-  const view = new View();
+function initilize(){
   const model = new Model(players);
+  const view = new View(model);
+  const controller = new Controller(view, model);
 
-  view.bindGameResetEvent(event => {
-    event.preventDefault();
-    view.closeNotification();
-    model.reset();
-    view.clearMoves();
-    view.setTurnIndicator(model.game.currentPlayer);
-    view.updateScoreBord(model.stats.playerWithStats[0].wins, model.stats.playerWithStats[1].wins, model.stats.draw)
-  });
-
-  view.bindNewRoundEvent(event => {
-    event.preventDefault();
-    model.newRound();
-    view.closeNotification();
-    view.clearMoves();
-    view.setTurnIndicator(model.game.currentPlayer);
-    view.updateScoreBord(model.stats.playerWithStats[0].wins, model.stats.playerWithStats[1].wins, model.stats.draw)
-  });
-
-  view.bindPlayerMoveEvent((box) => {
-
-    const existingMove = model.game.moves.find((move) => move.boxId === +box.id);
-
-    if (existingMove){
-      return;
-    }
-
-    //Place an icon of the current player in a box
-    view.handlePlayerMove(box, model.game.currentPlayer);
-
-    //Advance to the next state by pushing a move to the moves array
-    model.playerMove(+box.id)
-
-    if (model.game.status.isComplete){
-
-      view.openNotification(model.game.status.winner ? `${model.game.status.winner.name} wins!` : 'It is Draw!')
-      return
-    }
-
-    //Set the next player's turn indicator
-    view.setTurnIndicator(model.game.currentPlayer);
-
-
-  });
+  controller.init();
 
 }
 
-window.addEventListener("load", init);
+window.addEventListener("DOMContentLoaded", initilize);
+
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,153 +1,59 @@
+export default class Controller {
+  constructor(view, model){
+    this.model = model;
+    this.view = view;
+  };
 
-const App ={
-
-  $:{
-
-    resetBtn: document.querySelector('[data-id="reset-button"]'),
-    newGameBtn: document.querySelector('[data-id="new-game-button"]'),
-    boxes: document.querySelectorAll('[data-id="box"]'),
-    notification: document.querySelector('[data-id="notification-holder"]'),
-    playerWonText: document.querySelector('[data-id="player-won"]'),
-    playAgainBtn: document.querySelector('[data-id="play-again-btn"]'),
-    turn: document.querySelector('[data-id="x-turn"]'),
-
-  },
-
-  state:{
-
-    moves: [],
-
-  },
-
-  getGameStatus(moves){
-    
-    const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.boxId)
-    const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.boxId)
-
-    const winningPatterns = [
-      [1, 2, 3],
-      [1, 5, 9],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-      [3, 5, 7],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
-
-    let winner = null
-    winningPatterns.forEach(pattern => {
-
-
-      const p1Wins = pattern.every(v => p1Moves.includes(v))
-      const p2Wins = pattern.every(v => p2Moves.includes(v))
-
-      if (p1Wins) winner = 1
-      if (p2Wins) winner = 2
-
-    })
-
-    return{
-      status: moves.length === 9 || winner != null ? 'complete' : 'in-progress', //in-progress or complete
-      winner  // 1 or 2 or null
-
-    }
-  },
-
-  init(){
-    App.registerEvents()
-  },
-
-  registerEvents(){
-    console.log(App.$.boxes)
-
-    App.$.resetBtn.addEventListener("click", (event) => {
+  
+  
+  init() {
+  
+    this.view.bindGameResetEvent(event => {
       event.preventDefault();
-      console.log('Reset the game');
+      this.view.closeNotification();
+      this.model.reset();
+      this.view.clearMoves();
+      this.view.setTurnIndicator(this.model.game.currentPlayer);
+      this.view.updateScoreBord(this.model.stats.playerWithStats[0].wins, this.model.stats.playerWithStats[1].wins, this.model.stats.draw)
     });
-
-    App.$.newGameBtn.addEventListener("click", (event) => {
+  
+    this.view.bindNewRoundEvent(event => {
       event.preventDefault();
-      console.log('Start a new game');
+      this.model.newRound();
+      this.view.closeNotification();
+      this.view.clearMoves();
+      this.view.setTurnIndicator(this.model.game.currentPlayer);
+      this.view.updateScoreBord(this.model.stats.playerWithStats[0].wins, this.model.stats.playerWithStats[1].wins, this.model.stats.draw)
     });
-
-    App.$.playAgainBtn.addEventListener("click", event => {
-      App.state.moves = [];
-      App.$.boxes.forEach(box => box.replaceChildren());
-      App.$.notification.classList.add('hidden')
+  
+    this.view.bindPlayerMoveEvent((box) => {
+  
+      const existingMove = this.model.game.moves.find((move) => move.boxId === +box.id);
+  
+      if (existingMove){
+        return;
+      }
+  
+      //Place an icon of the current player in a box
+      this.view.handlePlayerMove(box, this.model.game.currentPlayer);
+  
+      //Advance to the next state by pushing a move to the moves array
+      this.model.playerMove(+box.id)
+  
+      if (this.model.game.status.isComplete){
+  
+        this.view.openNotification(this.model.game.status.winner ? `${this.model.game.status.winner.name} wins!` : 'It is Draw!')
+        return
+      }
+  
+      //Set the next player's turn indicator
+      this.view.setTurnIndicator(this.model.game.currentPlayer);
+  
+  
     });
-
-    App.$.boxes.forEach(box => {
-      box.addEventListener("click", (event) => {
-
-        //Check if there is already a play, if so, return early
-
-        const hasMove = (boxId) => {
-          const existingMove = App.state.moves.find(move => move.boxId === boxId)
-          return existingMove !== undefined
-        }
-
-        if (hasMove(+box.id)){
-          return;
-        };
-
-        //Determine which player icon to add to the box
-
-        const lastMove = App.state.moves.at(-1);
-
-        const getoppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
-
-        const currentPlayer = App.state.moves.length === 0? 1: getoppositePlayer(lastMove.playerId);
-
-        const nextPlayer = getoppositePlayer(currentPlayer)
-
-        const icon = document.createElement("i");
-        const turnIcon = document.createElement("i");
-        const turnLabel = document.createElement('p');
-        turnLabel.innerText = `Player ${nextPlayer}, you are up!`
-
-        if (currentPlayer === 1){
-          icon.classList.add('fa-solid', 'fa-xmark')
-          turnIcon.classList.add('fa-solid', 'fa-o', 'yellow')
-          turnLabel.classList.add('yellow') 
-          
-        }else{
-          icon.classList.add('fa-solid', 'fa-o')
-          turnIcon.classList.add('fa-solid', 'fa-xmark', 'red')
-          turnLabel.classList.add('red')
-        } 
-
-        App.$.turn.replaceChildren(turnIcon, turnLabel);
-
-        App.state.moves.push({
-          boxId: +box.id,
-          playerId: currentPlayer,
-        });
-
-        box.replaceChildren(icon);
-
-
-        const game = App.getGameStatus(App.state.moves)
-        
-        if (game.status === 'complete'){
-
-          App.$.notification.classList.remove("hidden");
-
-          let message = ''
-          if (game.winner){
-            message = `Player ${game.winner} wins!`
-          }else {
-            message = `It Was Draw`
-          }
-
-          App.$.playerWonText.textContent = message;
-        }
-
-      });
-    });
+  
   }
-
+  
 }
 
-window.addEventListener("load", App.init);
-
+// window.addEventListener("load", init);
